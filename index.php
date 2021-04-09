3f208ba28924a9279921f35224fb8b54f3b77f5e
CodeRangerMain <82121053+CodeRangerMain@users.noreply.github.com>
2021-04-10 01:12:52 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,122 @@
+ <?php error_reporting(0);
+		include './admin/config.php';
+		$config = getConfig();
+		$page = 'index';
+		?>
+<!doctype html>
+<html>
+<head><?php if (!empty($config)) include "./admin/include-head.php" ?>
+<meta charset="utf-8">
+<title>Агентство эффективной интернет-рекламы</title>
+<meta property="og:image" content="images/Kviz_1.png">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="favicon.ico" rel="shortcut icon" type="image/x-icon">
+<link href="css/base/jquery-ui.min.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Montserrat:700" rel="stylesheet">
+<link href="css/oknavdom.css" rel="stylesheet">
+<link href="css/index.css" rel="stylesheet">
+<script src="js/jquery-1.12.4.min.js"></script>
+<script src="js/jquery-ui.min.js"></script>
+<link href="mystyle.css" rel="stylesheet">
+<link rel="stylesheet" href="css/reset.css">	
+<link rel="stylesheet" href="css/media.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.css"/>	
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.js"></script>	
+<!-- arcticmodal -->	
+<link rel="stylesheet" href="libs/arcticmodal/jquery.arcticmodal-0.3.css">	
+<script src="libs/arcticmodal/jquery.arcticmodal-0.3.min.js">   
+</script>	
+<!-- slick-slider -->	
+<link rel="stylesheet" href="libs/slick-slider/slick.css">	
+<script src="libs/slick-slider/slick.min.js"></script>
+
+<link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700&display=swap&subset=cyrillic" rel="stylesheet">
+
+<script src="js/cartonbox.min.js"></script>      
+
+<script src="js/index.js"></script>
+</head>
+<body><?php if (!empty($config)) include "./admin/include-body-start.php" ?>
+<div id="Layer1">
+<div id="Layer1_Container">
+<div id="Html1">
+<div class="our-works">
+		<div class="container">
+			<div class="slider-container">
+				<div class="our-works-slider">
+					<div class="slide"><a data-fancybox="gallery" href="img/gallery/1.jpg"><img src="img/gallery/1.jpg" alt=""></a></div>
+					<div class="slide"><a data-fancybox="gallery" href="img/gallery/2.jpg"><img src="img/gallery/2.jpg" alt=""></a></div>
+					<div class="slide"><a data-fancybox="gallery" href="img/gallery/3.png"><img src="img/gallery/3.png" alt=""></a></div>
+					<div class="slide"><a data-fancybox="gallery" href="img/gallery/4.png"><img src="img/gallery/4.png" alt=""></a></div>
+					<div class="slide"><a data-fancybox="gallery" href="img/gallery/5.png"><img src="img/gallery/5.png" alt=""></a></div>
+					<div class="slide"><a data-fancybox="gallery" href="img/gallery/6.png"><img src="img/gallery/6.png" alt=""></a></div>
+					<div class="slide"><a data-fancybox="gallery" href="img/gallery/7.png"><img src="img/gallery/7.png" alt=""></a></div>
+					<div class="slide"><a data-fancybox="gallery" href="img/gallery/8.png"><img src="img/gallery/8.png" alt=""></a></div>
+					<div class="slide"><a data-fancybox="gallery" href="img/gallery/9.png"><img src="img/gallery/9.png" alt=""></a></div>			
+				</div>
+			</div>
+		</div>
+	</div></div>
+<div id="wb_Text1">
+<span id="wb_uid0"></span></div>
+<div id="wb_Image2">
+<img src="images/phone.png" id="Image2" alt=""></div>
+<div id="wb_Text2">
+<a href="tel:88005511808"><span id="wb_uid1">8 (800) 551-18-08</span></a></div>
+<div id="wb_Image3">
+<img src="images/elipse.png" id="Image3" alt=""></div>
+<div id="wb_Image1">
+<img src="/images/logo.svg" id="Image1" alt=""></div>
+<div id="wb_Text3">
+<span id="wb_uid2">Пн–Пт: с 9:00-18:00<br>Сб, Вс: выходные дни</span></div>
+<div id="wb_Text4">
+<span id="wb_uid3">Увеличим колличество лидов в 2-3 раза и 
+ </span><span id="wb_uid4"> СНИЗИМ РАСХОДЫ </span><span id="wb_uid5">на рекламу</span></div>
+<div id="wb_Image4">
+<img src="images/trigger-1.png" id="Image4" alt=""></div>
+<div id="wb_Text5">
+<span id="wb_uid6">Аудит вашего текущего сайта</span></div>
+<div id="wb_Image5">
+<img src="images/trigger-1.png" id="Image5" alt=""></div>
+<div id="wb_Text6">
+<span id="wb_uid7">Кейсы по вашей нише</span></div>
+<div id="wb_Image6">
+<img src="images/trigger-1.png" id="Image6" alt=""></div>
+<div id="wb_Text7">
+<span id="wb_uid8">Более 200 успешных проектов</span></div>
+<div id="wb_Text8">
+<span id="wb_uid9">Оставьте заявку на создание квиза через сайт и </span><span id="wb_uid10">получите 1 из 3 подарков</span></div>
+<a data-type="iframe" data-src="test/index.php" href="javascript:;" class="main-btn"><button type="submit" id="ThemeableButton1" name="button" value="ОСТАВИТЬ ЗАЯВКУ" class="butt-anim">ОСТАВИТЬ ЗАЯВКУ</button></a>
+<div id="wb_Text9">
+<span id="wb_uid12">Тест бесплатный. Данные защищены</span></div>
+<div id="wb_Image9">
+<img src="images/bussiness.png" id="Image9" alt=""></div>
+<div id="wb_Image10">
+<img src="images/snoska.png" id="Image10" alt=""></div>
+<div id="wb_Image12">
+<img src="images/snoska.png" id="Image12" alt=""></div>
+<div id="wb_Image15">
+<img src="images/snoska.png" id="Image15" alt=""></div>
+<div id="wb_Text10">
+</div>
+<div id="wb_Text11">
+<span id="wb_uid14">С 2013 года разработали и запустили </span><span id="wb_uid15">более 100 сайтов</span></div>
+<div id="wb_Text12">
+<span id="wb_uid16">Посмотрите на последние работы</span></div>
+<a data-type="iframe" data-src="test/index.php" href="javascript:;" class="main-btn"><button type="submit" id="ThemeableButton2" name="button" value="ОСТАВИТЬ ЗАЯВКУ СЕЙЧАС" class="butt-anim">ОСТАВИТЬ ЗАЯВКУ СЕЙЧАС</button></a>
+<div id="wb_Image13">
+<img src="/images/logo.svg" id="Image13" alt=""></div>
+<div id="wb_Text14">
+<span id="wb_uid17">Варшавское шоссе, 132Ак1
+Москва, Россия, 117519</span></div>
+<div id="wb_Text15">
+<span id="wb_uid18"><a data-type="iframe" data-src="politika-konfidencialnosti.html" href="javascript:;" class="main-btn"><span id="wb_uid18"><u>Политика конфиденциальности</u></span></a></div>
+<div id="wb_Image20">
+<img src="images/gift-box.svg" id="Image20" alt=""></div>
+<div id="wb_Image17">
+<img src="images/snoska.png" id="Image17" alt=""></div>
+
+
+<script src="js/main.js"></script><?php if (!empty($config)) include "./admin/include-body-end.php" ?>	
+</body>
+</html>
