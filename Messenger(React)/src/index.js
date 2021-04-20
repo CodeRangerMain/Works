119301f26d612c436a32b10e7237536bcb2523d2
CodeRangerMain <82121053+CodeRangerMain@users.noreply.github.com>
2021-04-21 00:06:14 +0300

    Add files via upload
    UPDATE STATE

--- Messenger(React)/src/index.js
+++ Messenger(React)/src/index.js
@@ -1,6 +1,23 @@
-import state from './components/Redux/state'
-import { renderDom } from './render';
+import storage from './components/Redux/state'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-renderDom(state)
 
+
+let renderDom = (state, addPost) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App state={state} addPost={addPost} />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+  reportWebVitals();
+}
+renderDom(storage.state, storage.addPost)
+storage.observer(renderDom);
+
+window.storage = storage
 export default renderDom;
